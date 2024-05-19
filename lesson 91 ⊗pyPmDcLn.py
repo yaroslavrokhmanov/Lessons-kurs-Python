6bcc3290006be232cbaf23c061ac2d4ccb994a20
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2024-05-19 11:01:38 +0200

    finish

--- lesson 91 ⊗pyPmDcLn.py
+++ lesson 91 ⊗pyPmDcLn.py
@@ -1,1 +1,20 @@
 # 1
+dct = {
+	'x': 1,
+	'y': 2,
+	'z': 3
+}
+print(len(dct)) 
+
+# 2
+dct1 = {
+	'a': 12,
+	'b': 34,
+	'c': 56,
+	'd': 78,
+	'e': 90
+}
+dct2 = {}
+len_dct1 = len(dct1)
+dct2['dct1']= len_dct1
+print(dct2)
