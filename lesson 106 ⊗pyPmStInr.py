c6b52f8627c2201bb64306ead7e89c422eaf9e9e
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2024-05-26 16:03:10 +0200

    finish

---
+++ lesson 106 ⊗pyPmStInr.py
@@ -0,0 +1,16 @@
+# # 1
+# tst = {}
+# print(type(tst)) # создается пустой словарь <class 'dict'>
+
+# 2
+tst = set()
+print(type(tst)) #создается пустое множество <class 'set'>
+
+# 3
+tst = {'x', 'y', 'z'}
+print(type(tst)) # в переменную tst записуется множество cо значениями, тип <class 'set'>
+
+# 4
+tst = {'a': 1, 'b': 2, 'c': 3}
+print(type(tst)) #<class 'set'> создали словарь так как присутствует ключ-значение
+
