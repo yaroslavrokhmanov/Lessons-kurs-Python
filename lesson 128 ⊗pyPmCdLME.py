94671ba21e7c93863780d99f434a907ed21b240d
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2024-06-09 18:31:35 +0200

    finish

---
+++ lesson 128 ⊗pyPmCdLME.py
@@ -0,0 +1,41 @@
+# 1
+tst = 5
+if tst > 10:
+    print('true')
+else:
+    print('false') # false
+
+# 2
+tst = 5
+if tst < 10:
+    print('true') # true
+else:
+    print('false') 
+
+# 3
+tst = 5
+if tst >= 5:
+    print('true') # true
+else:
+    print('false') 
+
+# 4
+tst = 5
+if tst <= -5:
+    print('true') 
+else:
+    print('false') # false 
+
+# 5
+tst = 5
+if tst  > 10:
+    print('true') 
+else:
+    print('false') # false 
+
+# 6
+age = 46
+if age >= 18:
+	print('You have access to the site') # You have access to the site
+else:
+	print('Нou do not have access to the site')
