9088c840802f1cfbffc6c25c37614f7afef103a5
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2025-01-19 18:49:51 +0100

    finish

---
+++ lesson 1.2 ⊗pyPmFnGVS .py
@@ -0,0 +1,15 @@
+# 1.Каким будет результат выполнения следующего кода:
+num = 2
+
+def func():
+	return num
+
+func()
+print(num) # 2
+
+# 2.Каким будет результат выполнения следующего кода:
+# num = 2
+
+# def func():
+# 	return num1
+# print(func()) # oshibka
