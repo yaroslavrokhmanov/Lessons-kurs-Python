41cc4c48fd77ba5d6f7f4c5da5837e89ae46a012
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2025-02-06 21:23:09 +0100

    finish

--- lesson 1.24 ⊗pyPmFnRe .py
+++ lesson 1.24 ⊗pyPmFnRe .py
@@ -1,5 +1,5 @@
 # 1.Выведите все числовые элементы словаря произвольного уровня вложенности:
-{
+elems = {
 	'a': {
 		'b': 1,
 		'c': 2,
@@ -19,7 +19,65 @@ def func(elem):
     res = []
     for key, value in elem.items():
         if isinstance(value, dict):
-            func(elem)
+            res.extend(func(value))
         else: 
-            res.append(value)
+            if isinstance(value, (int)):
+                res.append(value)
     return res
+
+print(func(elems)) #[1, 2, 3, 4, 5, 6, 7]
+
+# 2.Найдите сумму элементов словаря из предыдущей задачи.
+elems = {
+	'a': {
+		'b': 1,
+		'c': 2,
+		'd': {
+			'e': 3,
+			'f': 4
+		}
+	},
+	'j': {
+		'h': 5,
+		'k': 6,
+	},
+	'l': 7
+}
+
+def sum_elems(num):
+    res = 0
+    for key, value in num.items():
+        if isinstance(value, dict):
+            res += sum_elems(value)
+        elif isinstance(value, int):
+            res += value
+    return res
+print(sum_elems(elems)) # 28
+
+# 3.Получите список примитивных элементов словаря из предыдущей задачи.
+elems = {
+	'a': {
+		'b': 1,
+		'c': 2,
+		'd': {
+			'e': 3,
+			'f': 4
+		}
+	},
+	'j': {
+		'h': 5,
+		'k': 6,
+	},
+	'l': 7
+}
+
+def primitiv_elems(el):
+    res = []
+    for key, value in el.items():
+        if isinstance(value, (int, float, str, bool, type(None))):
+            res.append(value)
+        elif isinstance(value, dict):
+            res.extend(primitiv_elems(value))
+    return res
+
+print(primitiv_elems(elems)) # [1, 2, 3, 4, 5, 6, 7]
