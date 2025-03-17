228427f39ff8ce592749c16864e692fc445083f9
Yaroslav Rokhmanov <yaroslavrokhmanov@gmail.com>
2025-03-17 20:36:59 +0100

    finish

---
+++ lesson 1.77 ⊗pyPmExcBT .py
@@ -0,0 +1,11 @@
+# Дан следующий код:
+txt = '2'
+try:
+    res1 = txt + 3
+except TypeError:
+    print('error2: skladywaem stroku s czislom')
+
+try:
+    res2 = txt1
+except NameError:
+    print('error1: nesuszestwujuszaja peremennaja txt1')
